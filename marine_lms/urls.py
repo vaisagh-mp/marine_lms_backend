6b288d76184d7d47edf2c5513aa581b99eb20233
PontusDigimark <contact@pontusdigi.com>
2025-11-24 15:32:32 +0530

    course detail api

--- marine_lms/urls.py
+++ marine_lms/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,5 @@ urlpatterns = [
     path('api/courses/', include('courses.urls')),
     path('api/progress/', include('progress.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
